703b38b18744a9acce4bea6ce2fca8f0534f254b
Nate Barlow <nathanbarlow23@gmail.com>
2019-01-31 19:41:18 -0700

    Added more functionality to register service

--- server/src/main/java/cs340/TicketToRide/model/IServerModel.java
+++ server/src/main/java/cs340/TicketToRide/model/IServerModel.java
@@ -1,9 +1,12 @@
 package cs340.TicketToRide.model;
 
 import cs340.TicketToRide.utility.ID;
+import cs340.TicketToRide.utility.Username;
 
 public interface IServerModel {
     User getUserByAuthToken(AuthToken token);
     void addGame(Game game);
     Game getGameByID(ID gameID);
+    User getUserByUsername(Username username);
+    void registerUser(User user, AuthToken token);
 }
--- server/src/main/java/cs340/TicketToRide/model/ServerModel.java
+++ server/src/main/java/cs340/TicketToRide/model/ServerModel.java
@@ -1,15 +1,22 @@
 package cs340.TicketToRide.model;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 import cs340.TicketToRide.utility.ID;
+import cs340.TicketToRide.utility.Username;
 
 public class ServerModel implements IServerModel {
     private static ServerModel singleton;
     private AuthManager authManager;
     private Games games;
+    private Set<User> users;
 
     private ServerModel() {
         authManager = new AuthManager();
         games = new Games();
+        users = new HashSet<>();
     }
 
     public static ServerModel getInstance() {
@@ -41,5 +48,38 @@ public class ServerModel implements IServerModel {
 
     public Game getGameByID(ID gameID) {
         return games.getGameByID(gameID);
+    }
+
+    public User getUserByUsername(Username username) {
+        if (username == null || !username.isValid()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public void registerUser(User user, AuthToken token) {
+        if (user == null || token == null || !user.isValid() || !token.isValid()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (users.contains(user)) {
+            // todo: throw exception?
+            return;
+        }
+
+        if (authManager.getUserByAuthToken(token) != null) {
+            // todo: throw exception?
+            return;
+        }
+
+        users.add(user);
+        authManager.addTokenUser(token, user);
     }
 }
