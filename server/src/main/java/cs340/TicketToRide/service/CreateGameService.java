3fa405b2d791e0c190525ba0024379d5420a6e85
Nate Barlow <nathanbarlow23@gmail.com>
2019-01-31 17:50:37 -0700

    Some more basic service functionality

--- server/src/main/java/cs340/TicketToRide/service/CreateGameService.java
+++ server/src/main/java/cs340/TicketToRide/service/CreateGameService.java
@@ -5,6 +5,9 @@ import cs340.TicketToRide.model.Game;
 
 public class CreateGameService {
     public Game createGame(AuthToken token) {
+        if (token == null || !token.isValid()) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 }
