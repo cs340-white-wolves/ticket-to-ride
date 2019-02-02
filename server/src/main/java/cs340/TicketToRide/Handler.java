ef7a026c8e4c00a1c4b251b284c41a74505e2b69
Brendon Jensen <brendonjensen@gmail.com>
2019-02-01 20:14:32 -0700

    Start work on handler

--- server/src/main/java/cs340/TicketToRide/Handler.java
+++ server/src/main/java/cs340/TicketToRide/Handler.java
@@ -4,10 +4,25 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Locale;
 
 public class Handler implements HttpHandler{
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        if (httpExchange.getRequestMethod() != "POST") {
+            sendError(httpExchange, 405, "Method Not Supported");
+            return;
+        }
+
         // TODO: stub
+    }
+
+    private void sendError(HttpExchange httpExchange, int err, String msg) throws IOException {
+        String realMsg = String.format(Locale.US,"<h1>%d %s</h1>", err, msg);
+        httpExchange.sendResponseHeaders(err, realMsg.length());
+        OutputStreamWriter osw = new OutputStreamWriter(httpExchange.getResponseBody());
+        osw.write(realMsg);
+        osw.close();
     }
 }
