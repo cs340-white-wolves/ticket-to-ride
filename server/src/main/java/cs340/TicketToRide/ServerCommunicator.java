098683839db52e78ce275ebcf1e04cce90d00707
Brendon Jensen <brendonjensen@gmail.com>
2019-02-01 20:01:39 -0700

    Implement ServerCommunicator with logger

--- server/src/main/java/cs340/TicketToRide/ServerCommunicator.java
+++ server/src/main/java/cs340/TicketToRide/ServerCommunicator.java
@@ -1,4 +1,69 @@
 package cs340.TicketToRide;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.logging.*;
+
+
 public class ServerCommunicator {
+
+    private static final int MAX_CONNECTIONS = 10;
+    private static final int PORT = 8080;
+    private static final String PATH_COMMAND = "/command";
+
+    private static Logger logger;
+
+    static {
+        try {
+            initLog();
+        }
+        catch (IOException e) {
+            System.out.println("Could not initialize log: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void run () {
+        HttpServer server;
+
+        try {
+            server = HttpServer.create(new InetSocketAddress(PORT), MAX_CONNECTIONS);
+        }
+        catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            return;
+        }
+
+        server.setExecutor(null);
+
+        logger.info("Creating context");
+        server.createContext(PATH_COMMAND, new Handler());
+
+        logger.info("Starting HTTP server");
+        server.start();
+    }
+
+    public static void main(String[] args) {
+        new ServerCommunicator().run();
+    }
+
+    private static void initLog() throws IOException {
+        Level logLevel = Level.FINEST;
+
+        logger = Logger.getLogger("tickettoride");
+        logger.setLevel(logLevel);
+        logger.setUseParentHandlers(false);
+
+        java.util.logging.Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(logLevel);
+        consoleHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(consoleHandler);
+
+        FileHandler fileHandler = new FileHandler("log.txt", false);
+        fileHandler.setLevel(logLevel);
+        fileHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(fileHandler);
+    }
 }
