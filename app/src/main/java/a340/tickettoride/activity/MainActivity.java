9e77bbbaf5ec34c4ce9ef1c5ed18451782785549
rsweston <raygunrocks@gmail.com>
2019-02-04 22:34:53 -0700

    MainActivity and LoginActivity implementation; additions to ILobbyPresenter

--- app/src/main/java/a340/tickettoride/activity/MainActivity.java
+++ app/src/main/java/a340/tickettoride/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package a340.tickettoride.activity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,45 +9,56 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import a340.tickettoride.R;
+import a340.tickettoride.presenter.MainPresenter;
 import cs340.TicketToRide.model.User;
 
 public class MainActivity extends AppCompatActivity {
-    String mUsername = "";
-    String mPassword = "";
-    EditText mUsernameField;
-    EditText mPasswordField;
-    Button mLoginButton = null;
-    Button mRegisterButton = null;
+    private MainPresenter presenter;
+    private String mUsername = "";
+    private String mPassword = "";
+    private Button mLoginButton = null;
+    private Button mRegisterButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        presenter = new MainPresenter();
+
         // login button wire-up & listener
         mLoginButton = findViewById(R.id.loginButton);
-        mLoginButton.setEnabled(false);
+        mLoginButton.setEnabled(true);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                try {
+                    presenter.login(mUsername, mPassword);
+                } catch (Exception ex) {
+                    // handle exception (should this be handled in presenter?)
+                }
             }
         });
 
         // register button wire-up & listener
         mRegisterButton = findViewById(R.id.registerButton);
-        mRegisterButton.setEnabled(false);
+        mRegisterButton.setEnabled(true);
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                try {
+                    presenter.register(mUsername, mPassword);
+                } catch (Exception ex) {
+                    // handle exception (should this be handled in presenter?)
+                }
             }
         });
 
         // username field wire-up & listener
-        mUsernameField = findViewById(R.id.usernameInput);
+        EditText mUsernameField = findViewById(R.id.usernameInput);
         mUsernameField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -56,13 +68,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 mUsername = s.toString();
-                if (canLogin()) {
-                    mLoginButton.setEnabled(true);
-                    mRegisterButton.setEnabled(true);
-                } else {
-                    mLoginButton.setEnabled(false);
-                    mRegisterButton.setEnabled(false);
-                }
+//                if (canLogin()) {
+//                    mLoginButton.setEnabled(true);
+//                    mRegisterButton.setEnabled(true);
+//                } else {
+//                    mLoginButton.setEnabled(false);
+//                    mRegisterButton.setEnabled(false);
+//                }
             }
 
             @Override
@@ -72,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // password field wire-up & listener
-        mPasswordField = findViewById(R.id.passwordInput);
+        EditText mPasswordField = findViewById(R.id.passwordInput);
         mPasswordField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -82,13 +94,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 mPassword = s.toString();
-                if (canLogin()) {
-                    mLoginButton.setEnabled(true);
-                    mRegisterButton.setEnabled(true);
-                } else {
-                    mLoginButton.setEnabled(false);
-                    mRegisterButton.setEnabled(false);
-                }
+//                if (canLogin()) {
+//                    mLoginButton.setEnabled(true);
+//                    mRegisterButton.setEnabled(true);
+//                } else {
+//                    mLoginButton.setEnabled(false);
+//                    mRegisterButton.setEnabled(false);
+//                }
             }
 
             @Override
@@ -98,15 +110,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void loggedIn(User user) {
-
+    public void loggedIn() {
+        // start LobbyActivity
+        Intent intent = new Intent(MainActivity.this, LobbyActivity.class);
+        startActivity(intent);
     }
 
-    public void invalid() {
-
+    public void invalid(String message) {
+        showMessage(message); // pop-up indicating what went wrong
     }
 
-    private boolean canLogin() {
-        return (!mUsername.equals("") && !mPassword.equals(""));
+    private void showMessage(String message) {
+        Toast.makeText(this,
+                message,
+                Toast.LENGTH_SHORT).show();
     }
+
+//    private boolean canLogin() {
+//        return (!mUsername.equals("") && !mPassword.equals(""));
+//    }
 }
