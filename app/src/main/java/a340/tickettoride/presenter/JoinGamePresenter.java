368e6ba714d933f792278a9e90f3a7e15dea7539
rsweston <raygunrocks@gmail.com>
2019-02-06 14:48:53 -0700

    started JoinGameActivity

--- app/src/main/java/a340/tickettoride/presenter/JoinGamePresenter.java
+++ app/src/main/java/a340/tickettoride/presenter/JoinGamePresenter.java
@@ -1,9 +1,16 @@
 package a340.tickettoride.presenter;
 
 import a340.tickettoride.task.JoinGameTask;
+import cs340.TicketToRide.model.Game;
 import cs340.TicketToRide.utility.ID;
 
 public class JoinGamePresenter implements IJoinGamePresenter {
+    private View view;
+
+    public JoinGamePresenter(View view) {
+        setView(view);
+    }
+
     @Override
     public void joinGame(ID gameID) {
         if (gameID == null || !gameID.isValid()) {
@@ -15,6 +22,10 @@ public class JoinGamePresenter implements IJoinGamePresenter {
     }
 
     public interface View {
+        void gameJoined();
+    }
 
+    public void setView(View view) {
+        this.view = view;
     }
 }
