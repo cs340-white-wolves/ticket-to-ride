b3732818b7468d3d4256949768dd6d3982e067be
Curt Gilbert <cgilbertut@gmail.com>
2019-03-02 21:37:58 -0700

    Added the draw pile

--- app/src/main/java/a340/tickettoride/fragment/left/BankFragment.java
+++ app/src/main/java/a340/tickettoride/fragment/left/BankFragment.java
@@ -1,84 +1,108 @@
 package a340.tickettoride.fragment.left;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 import a340.tickettoride.R;
+import a340.tickettoride.presenter.BankPresenter;
+import a340.tickettoride.presenter.IBankPresenter;
+import cs340.TicketToRide.model.game.card.TrainCard;
+
+public class BankFragment extends Fragment implements BankPresenter.BankPresenterListener {
 
 
-public class BankFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private BankInteractionListener mListener;
-
-    public BankFragment() {
-        // Required empty public constructor
-    }
-
-    public static BankFragment newInstance(String param1, String param2) {
-        BankFragment fragment = new BankFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private IBankPresenter presenter;
+    private ImageView[] faceUpCards = new ImageView[5];
+    private TextView drawPile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bank, container, false);
+        View newView = inflater.inflate(R.layout.fragment_bank, container, false);
+        presenter = new BankPresenter(this);
+        bindViews(newView);
+        setClickListeners();
+        updateFaceUpCards(presenter.getCurrentFaceUpCards());
+        return newView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private void bindViews(View view) {
+        drawPile = view.findViewById(R.id.drawPile);
+        faceUpCards[0] = view.findViewById(R.id.card1);
+        faceUpCards[1] = view.findViewById(R.id.card2);
+        faceUpCards[2] = view.findViewById(R.id.card3);
+        faceUpCards[3] = view.findViewById(R.id.card4);
+        faceUpCards[4] = view.findViewById(R.id.card5);
+
+        for (int i = 0; i < 5; i++) {
+            faceUpCards[i].setTag(i);
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof BankInteractionListener) {
-            mListener = (BankInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement BankInteractionListener");
+
+    private int getCardResource(TrainCard.Color color) {
+        int resourceId;
+        switch (color) {
+            case passengerWhite: resourceId = R.drawable.white_car;
+                break;
+            case tankerBlue: resourceId = R.drawable.blue_car;
+                break;
+            case reeferYellow: resourceId = R.drawable.yellow_car;
+                break;
+            case freightOrange: resourceId = R.drawable.orange_car;
+                break;
+            case cabooseGreen: resourceId = R.drawable.green_car;
+                break;
+            case boxPurple: resourceId = R.drawable.pink_car;
+                break;
+            case hopperBlack: resourceId = R.drawable.black_car;
+                break;
+            case coalRed: resourceId = R.drawable.red_car;
+                break;
+            default: resourceId = R.drawable.locomotive;
+                break;
+        }
+        return resourceId;
+    }
+
+    private void setClickListeners() {
+
+        drawPile.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "You drew a new card",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        for (int i=0; i < 5; i++) {
+            faceUpCards[i].setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View view) {
+                    TrainCard newCard = presenter.drawTrainCard();
+                    ImageView card = (ImageView) view;
+                    card.setImageDrawable(getResources().getDrawable(getCardResource(newCard.getColor()),null));
+                }
+            });
         }
     }
 
+
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-
-    public interface BankInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void updateFaceUpCards(List<TrainCard> cards) {
+        for (int i=0; i < 5; i++) {
+            faceUpCards[i].setImageDrawable(getResources().getDrawable(getCardResource(cards.get(i).getColor()),null));
+        }
     }
 }
