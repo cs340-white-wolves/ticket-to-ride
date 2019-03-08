a51a2c23656bdcd08b38c05727e070e694fb0ce2
Curt Gilbert <cgilbertut@gmail.com>
2019-03-07 21:20:27 -0700

    Working on creating a test

---
+++ app/src/test/java/a340/tickettoride/model/ClientModelTest.java
@@ -0,0 +1,22 @@
+package a340.tickettoride.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientModelTest {
+
+    @Test
+    public void getActiveGame() {
+        ClientModel model = ClientModel.getInstance();
+        model.getActiveGame().
+    }
+
+    @Test
+    public void getPlayerId() {
+    }
+
+    @Test
+    public void getPlayerFromGame() {
+    }
+}
