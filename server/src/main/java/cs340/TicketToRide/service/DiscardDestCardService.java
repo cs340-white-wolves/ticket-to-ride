268f9ff10548e1b2a2739bd7530c66c235263399
Brendon Jensen <brendonjensen@gmail.com>
2019-03-31 14:42:36 -0600

    Fix order of a few things so that end game works properly

--- server/src/main/java/cs340/TicketToRide/service/DiscardDestCardService.java
+++ server/src/main/java/cs340/TicketToRide/service/DiscardDestCardService.java
@@ -41,23 +41,28 @@ public class DiscardDestCardService extends ActionService {
         String msg = "Drew " + (3 - discardCards.size()) + " destination cards";
         Message historyMessage = new Message(player.getUser().getUsername(), msg);
 
-        if (!game.allPlayersReady()) {
-            game.decrementPlayersLeftToDiscard();
-        } else {
-            game.setNextPlayerTurn();
-        }
-
         for (Player gamePlayer : game.getPlayers()) {
             IClient client = ClientProxyManager.getInstance().get(gamePlayer.getId());
             client.destCardDeckChanged(game.getDestCardDeck());
             client.playersUpdated(game.getPlayers());
             client.historyMessageReceived(historyMessage);
-            if (game.allPlayersReady()) {
-                client.setTurn(game.getCurrentPlayerTurnIdx());
-            }
         }
 
-        checkToEndGame(game);
+        boolean end = checkToEndGame(game);
 
+        if (! end) {
+            if (!game.allPlayersReady()) {
+                game.decrementPlayersLeftToDiscard();
+            } else {
+                game.setNextPlayerTurn();
+            }
+
+            if (game.allPlayersReady()) {
+                for (Player gamePlayer : game.getPlayers()) {
+                    IClient client = ClientProxyManager.getInstance().get(gamePlayer.getId());
+                    client.setTurn(game.getCurrentPlayerTurnIdx());
+                }
+            }
+        }
     }
 }
