e8d4749fd8e1eb99566caf7d55586d173474acaa
Brendon Jensen <brendonjensen@gmail.com>
2019-04-10 10:48:39 -0600

    Start stubbing out modules

---
+++ flatfile/src/main/java/cs340/TicketToRide/flatfile/GameDao.java
@@ -0,0 +1,49 @@
+package cs340.TicketToRide.flatfile;
+
+import cs340.TicketToRide.communication.Command;
+import cs340.TicketToRide.communication.Commands;
+import cs340.TicketToRide.model.Games;
+import cs340.TicketToRide.model.db.IGameDao;
+import cs340.TicketToRide.model.game.Game;
+
+public class GameDao implements IGameDao {
+    @Override
+    public void saveGame(Game game) {
+
+    }
+
+    @Override
+    public Games loadGames() {
+        return null;
+    }
+
+    @Override
+    public void saveCommand(Command command) {
+
+    }
+
+    @Override
+    public Commands loadCommands() {
+        return null;
+    }
+
+    @Override
+    public void clearCommands() {
+
+    }
+
+    @Override
+    public void beginTransaction() {
+
+    }
+
+    @Override
+    public void endTransaction() {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
